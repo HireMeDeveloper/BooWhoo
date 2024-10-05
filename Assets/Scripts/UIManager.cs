b38466190d06f9a6c0bdea39742414cfa7f14eb5
SAK70Games <138614585+SAK70Games@users.noreply.github.com>
2024-10-04 23:52:39 -0400

    Created initial UI for main and pause menus

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    public Canvas pauseMenuCanvas;
+
+    PlayerControls playerControls;
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        playerControls = new PlayerControls();
+    }
+
+    public void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    public void Disable()
+    {
+        playerControls.Disable();
+    }
+
+    public void Update()
+    {
+        if (playerControls.player.togglepause.ReadValue<float>() > 0.1f)
+            OpenPauseMenu();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ScottScene-Game");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
+    }
+
+    public void OpenPauseMenu()
+    {
+        pauseMenuCanvas.gameObject.SetActive(true);
+    }
+    
+    public void ClosePauseMenu()
+    {
+        pauseMenuCanvas.gameObject.SetActive(false);
+    }
+    public void ReturnToMainScene()
+    {
+        Destroy(GameObject.Find("MainObjects"));
+
+        SceneManager.LoadScene("ScottScene-Main");
+    }
+
+}
+
