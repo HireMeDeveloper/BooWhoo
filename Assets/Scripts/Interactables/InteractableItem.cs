c476aac720e7178dc12d8ddbb41484d740c5b1b1
acenettner <124236291+acenettner@users.noreply.github.com>
2024-10-15 20:24:03 -0700

    Added Interactable interface
    - Created 3 methods for interactable interface
    - Created PlayerInteractions script
    - Implemented interface methods in InteractableItem

--- Assets/Scripts/Interactables/InteractableItem.cs
+++ Assets/Scripts/Interactables/InteractableItem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
 
-public class InteractableItem : MonoBehaviour
+public class InteractableItem : MonoBehaviour, IInteractable
 {
     public Animator anim;
     // Check this in editor for empty searchables
@@ -15,19 +15,43 @@ public class InteractableItem : MonoBehaviour
         if (!opened) {
             anim.Play("Open");
             opened = true;
-            if (isEmpty) {
-                // TODO: Display dialogue
-                Debug.Log("It was empty...");
-            }
         }
     }
     
     void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.tag == "Player") {
-            if (!opened) {
-                Debug.Log("triggered");
-                anim.Play("Wiggle");
+            OnEnter(col.gameObject.GetComponent<PlayerInteraction>());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            OnExit(col.gameObject.GetComponent<PlayerInteraction>());
+        }
+    }
+
+    
+    // IInteractable methods
+    public void OnEnter(PlayerInteraction playerInteraction) {
+        playerInteraction.hoveredInteractables.Insert(0, gameObject);
+        if (!opened) {
+            anim.Play("Wiggle");
+        }
+    }
+    public void OnExit(PlayerInteraction playerInteraction) {
+        for (int i = 0; i < playerInteraction.hoveredInteractables.Count; i++) {
+            if (gameObject == playerInteraction.hoveredInteractables[i]) {
+                playerInteraction.hoveredInteractables.RemoveAt(i);
+                break;
             }
+        }
+    }
+    public void OnInteract(PlayerInteraction playerInteraction) {
+        playerInteraction.hoveredInteractables.RemoveAt(0);
+        if (gameObject.tag == "Item") {
+            Destroy(gameObject);
+        } else if (gameObject.tag == "Searchable") {
+            PlayOpenAnim();
         }
     }
 }
