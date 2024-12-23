43fe551ba5303b54ab31976812a92e025616d141
acenettner <124236291+acenettner@users.noreply.github.com>
2024-12-22 18:57:50 -0800

    Player update
    - Fixed player vertical input bug
    - Added sprite to player
    - Added idle animation
    - Added move animation
    - Added jump animation
    - Added land animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Components
+    public Animator anim;
+    public SpriteRenderer sr;
     PlayerInput playerInput;
     PlayerInteraction playerInteraction;
     Rigidbody2D rb;
@@ -14,6 +16,12 @@ public class PlayerController : MonoBehaviour
 
     float speed = 5.0f;
     float jumpForce = 400.0f;
+    bool inAir = false;
+
+    // Animation parameters
+    bool isMoving = false;
+    bool isJumping = false;
+    float xDir = 0.0f;
 
     void Start()
     {
@@ -27,18 +35,28 @@ public class PlayerController : MonoBehaviour
     {
         if (playerInput.actions["move"].IsPressed()) {
             PlayerMove();
+            isMoving = true;
         } else {
             // Stop moving in x direction if nothing is pressed
             rb.velocity = new Vector2(0, rb.velocity.y);
+            isMoving = false;
         }
 
-        if (playerInput.actions["jump"].WasPressedThisFrame() && IsGrounded()) {
-            PlayerJump();
+        if (IsGrounded()) {
+            if (playerInput.actions["jump"].WasPressedThisFrame()) {
+                PlayerJump();
+                isJumping = true;
+            }
+        } else {
+            inAir = true;
         }
+        Debug.Log(inAir);
 
         if (playerInput.actions["Interact"].WasPressedThisFrame() && playerInteraction.hoveredInteractables.Count > 0) {
             playerInteraction.Interact();
         }
+
+        Animate();
     }
 
     void PlayerMove() {
@@ -46,6 +64,7 @@ public class PlayerController : MonoBehaviour
         // We want only the x value from move since there is a separate button to jump
         float xVal = moveDirection.x;
         rb.velocity = new Vector2(xVal * speed, rb.velocity.y);
+        xDir = xVal;
     }
 
     void PlayerJump() {
@@ -61,6 +80,30 @@ public class PlayerController : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    void Animate() {
+        anim.SetFloat("xDir", xDir);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isJumping", isJumping);
+
+        if (isJumping) {
+            isJumping = false;
+        }
+
+        if (IsGrounded() && inAir) {
+            anim.SetBool("isLanding", true);
+            inAir = false;
+        } else if (anim.GetBool("isLanding")) {
+            anim.SetBool("isLanding", false);
+        }
+
+        // Flip sprite regardless of current animation
+        if (xDir > 0) {
+            sr.flipX = false;
+        } else if (xDir < 0) {
+            sr.flipX = true;
+        }
     }
 
 }
